c1805c075680de5966a2e8efc693f3731228c400
stuland-ir <github@stuland.ir>
2015-07-15 20:01:32 +0430

    Run Method By Name

---
+++ runMethodByName/startForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace runMethodByName
+{
+    partial class startForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txtMethodName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCp = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtArgs = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtClassName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(20, 179);
+            this.btnRun.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(377, 35);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // txtMethodName
+            // 
+            this.txtMethodName.Location = new System.Drawing.Point(130, 59);
+            this.txtMethodName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtMethodName.Name = "txtMethodName";
+            this.txtMethodName.Size = new System.Drawing.Size(267, 26);
+            this.txtMethodName.TabIndex = 1;
+            this.txtMethodName.Text = "TestMethod";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "method name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "cp :";
+            // 
+            // txtCp
+            // 
+            this.txtCp.Location = new System.Drawing.Point(130, 95);
+            this.txtCp.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCp.Name = "txtCp";
+            this.txtCp.Size = new System.Drawing.Size(267, 26);
+            this.txtCp.TabIndex = 3;
+            this.txtCp.Text = "constructor Param";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "input args :";
+            // 
+            // txtArgs
+            // 
+            this.txtArgs.Location = new System.Drawing.Point(130, 131);
+            this.txtArgs.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtArgs.Name = "txtArgs";
+            this.txtArgs.Size = new System.Drawing.Size(267, 26);
+            this.txtArgs.TabIndex = 5;
+            this.txtArgs.Text = "moo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "class name:";
+            // 
+            // txtClassName
+            // 
+            this.txtClassName.Location = new System.Drawing.Point(131, 23);
+            this.txtClassName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtClassName.Name = "txtClassName";
+            this.txtClassName.Size = new System.Drawing.Size(267, 26);
+            this.txtClassName.TabIndex = 7;
+            this.txtClassName.Text = "runMethodByName.Tester";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 237);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtClassName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtArgs);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMethodName);
+            this.Controls.Add(this.btnRun);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtMethodName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtArgs;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtClassName;
+    }
+}
+
---
+++ runMethodByName/startForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace runMethodByName
+{
+    public partial class startForm : Form
+    {
+        public startForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Invoker.CreateAndInvoke(txtClassName.Text, null, txtMethodName.Text, null);
+
+            Invoker.CreateAndInvoke(txtClassName.Text, new[] { txtCp.Text }, "TestMethodUsingValueFromConstructorAndArgs", new object[] { txtArgs.Text, false });
+        }
+    }
+
+    public static class Invoker
+    {
+        public static object CreateAndInvoke(string typeName, object[] constructorArgs, string methodName, object[] methodArgs)
+        {
+            try
+            {
+                Type type = Type.GetType(typeName);
+                object instance = Activator.CreateInstance(type, constructorArgs);
+
+                MethodInfo method = type.GetMethod(methodName);
+                return method.Invoke(instance, methodArgs);
+            }
+            catch (Exception Err)
+            {
+                MessageBox.Show(@"Method is not exists");
+                return null;
+            }
+        }
+    }
+
+
+    public class Tester
+    {
+        public string _testField;
+
+        public Tester()
+        {
+        }
+
+        public Tester(string arg)
+        {
+            _testField = arg;
+        }
+
+        public void TestMethod()
+        {
+            MessageBox.Show("Called TestMethod");
+        }
+
+        public void TestMethodWithArg(string arg)
+        {
+            MessageBox.Show("Called TestMethodWithArg: " + arg);
+        }
+
+        public void TestMethodUsingValueFromConstructorAndArgs(string arg, bool arg2)
+        {
+            MessageBox.Show("Called TestMethodUsingValueFromConstructorAndArg " + arg + " " + arg2 + " " + _testField);
+        }
+
+        public string GetContstructorValue()
+        {
+            return _testField;
+        }
+    }
+}
